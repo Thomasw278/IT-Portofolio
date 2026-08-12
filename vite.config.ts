b767e53ca164c4ffc5b541aed61ddb62c8d55cde
Thomas Wisnu <thomas.aquinas@ti.ukdw.ac.id>
2026-08-12 23:05:53 +0700

    Fix Path Vite Config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: './',
+  base: '/IT-Portofolio/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
